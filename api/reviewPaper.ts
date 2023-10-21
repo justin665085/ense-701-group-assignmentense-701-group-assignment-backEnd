03d07db1a7268aa7c9676cf0b04b885f81e0068a
justin665085 <justin665085@gmail.com>
2023-10-21 10:57:38 +0800

    finish reviewing all the paper and browsing all the analyzed paper

---
+++ api/reviewPaper.ts
@@ -0,0 +1,30 @@
+import { VercelRequest, VercelResponse } from '@vercel/node';
+import { MongoClient } from 'mongodb'
+const uri = "mongodb+srv://dbUser:dbUser@cluster0.kwhhqy3.mongodb.net/?retryWrites=true&w=majority";
+module.exports = async (req: VercelRequest, res: VercelResponse) => {
+    // @ts-ignore
+    const client = await MongoClient.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+    // @ts-ignore
+    const db = await client.db('ENSE');
+    const col = db.collection("ReviewedDocument");
+
+    // Create a new document
+    let Document = {
+        "title": req.body.title,
+        "authors": req.body.authors,
+        "year of publication": req.body.yop,
+        "jName": req.body.jName,
+        "SEpractice":req.body.SEpractice,
+        "claim": req.body.claim,
+        "result of evidence": req.body.ROE,
+        "type of research":req.body.TOR,
+        "type of participant": req.body.TOP,
+    }
+    // Insert the document into the specified collection
+    const p = await col.insertOne(Document);
+    // Find and return the document
+    const filter = {"title": req.body.title};
+    const deletedDocument = await db.collection("AnalyzedDocument").deleteMany(filter);
+    console.log("Document analyzed:\n" + JSON.stringify(deletedDocument));
+    res.status(200).json(deletedDocument);
+}
