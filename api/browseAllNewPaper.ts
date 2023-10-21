8e2752206e6b86e0b91c79c9525274210686a8ab
justin665085 <justin665085@gmail.com>
2023-10-21 10:26:15 +0800

    finish browsing all new paper

---
+++ api/browseAllNewPaper.ts
@@ -0,0 +1,14 @@
+import { VercelRequest, VercelResponse } from '@vercel/node';
+import { MongoClient } from 'mongodb'
+const uri = "mongodb+srv://dbUser:dbUser@cluster0.kwhhqy3.mongodb.net/?retryWrites=true&w=majority";
+module.exports = async (req: VercelRequest, res: VercelResponse) => {
+    // @ts-ignore
+    const client = await MongoClient.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+    // @ts-ignore
+    const db = await client.db('ENSE');
+    const col = db.collection("newAddDocument");
+
+    const document = await col.find().toArray();;
+    console.log("Document found:\n" + JSON.stringify(document));
+    res.status(200).json(document);
+}
